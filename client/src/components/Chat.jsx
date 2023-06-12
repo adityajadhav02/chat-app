b081393f1f32086dff9e90ef3c986e2fd486d824
Aditya Jadhav <jadhavaditya517@gmail.com>
2023-06-12 23:47:15 +0530

    select user

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { useContext } from 'react';
 import { useState, useEffect } from 'react'
 import Avatar from './Avatar';
+import Logo from './Logo';
+import { UserContext } from '../UserContext';
 
 const Chat = () => {
     const [ws, setWs] = useState(null);
     const [onlineUsers, setOnlineUsers] = useState({});
+    const [selectedUserId, setSelectedUserId] = useState(null);
+    const {username, id} = useContext(UserContext);
 
     useEffect(() => {
         const ws = new WebSocket('ws://localhost:8800');
@@ -30,32 +35,48 @@ const Chat = () => {
         }
     }
 
+    const otherUsers = {...onlineUsers};
+    delete otherUsers[id];
+
+
     return (
         <div className='flex h-screen'>
-            <div className="bg-white w-1/3 pt-4 pl-4">
-                <div className='text-2xl text-blue-600 font-bold flex gap-2 mb-4'>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                        <path d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 00-1.032-.211 50.89 50.89 0 00-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 002.433 3.984L7.28 21.53A.75.75 0 016 21v-4.03a48.527 48.527 0 01-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979z" />
-                        <path d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 001.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0015.75 7.5z" />
-                    </svg>
-                    SocialPost</div>
+            <div className="bg-white w-1/3">
+                <Logo />
                 {
-                    Object.keys(onlineUsers).map(userId => (
-                        <div className="flex items-center gap-4 border-b border-gray-300 py-3 cursor-pointer" >
+                    Object.keys(otherUsers).map(userId => (
+                        <div key={userId} onClick={() => setSelectedUserId(userId)}
+                        className={"flex items-center gap-4 border-b border-gray-300  cursor-pointer " + (userId===selectedUserId ? 'bg-blue-100' : '')} >
+                        {
+                            userId===selectedUserId && (
+                                <div className='w-1 bg-blue-500 h-16 rounded-r-md   '>
+                                </div>
+                            )
+                        }
+
+                        <div className='flex gap-2 py-3 pl-4 items-center'>
                             <Avatar 
-                                username = {onlineUsers[userId]}
-                                userId = {userId}
-                            />
-                            <span className='font-semibold text-lg'>
-                            {onlineUsers[userId]}
-                            </span>
+                                    username = {onlineUsers[userId]}
+                                    userId = {userId}
+                                />
+                                <span className='font-semibold text-lg'>
+                                {onlineUsers[userId]}
+                                </span>
+                        </div>
+                            
                         </div>
                     ))
                 }
 
             </div>
             <div className="bg-blue-100 w-2/3 p-2 flex flex-col">
-                <div className="flex-grow messages">Chat with selected user</div>
+                <div className="flex-grow messages">
+                    {!selectedUserId && (
+                        <div className='flex flex-col items-center justify-center h-full'>
+                            <div className='text-gray-500 text-md'>&larr; Select a user to chat</div>
+                        </div>
+                    )}
+                </div>
                 <div className='flex gap-2'>
                     <input type="text" placeholder='Type your message here'
                         className='bg-white p-2 flex-grow border outline-blue-500 rounded-md' />
